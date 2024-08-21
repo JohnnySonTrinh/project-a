300a6da58c5199ec1c3e2946835a160bd43095ab
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-08-22 01:58:10 +0200

    Add functionality to insert empty parentheses in code input with "(" key

--- assets/scripts/typing.js
+++ assets/scripts/typing.js
@@ -85,5 +85,20 @@ codeInput.addEventListener("keydown", (e) => {
 
     // Move the cursor position after the inserted spaces
     codeInput.selectionStart = codeInput.selectionEnd = start + 2;
+  } else if (e.key === "(") {
+    e.preventDefault();
+
+    // Get the current cursor position
+    const start = codeInput.selectionStart;
+    const end = codeInput.selectionEnd;
+
+    // Insert curly braces at the cursor position
+    codeInput.value =
+      codeInput.value.substring(0, start) +
+      "()" +
+      codeInput.value.substring(end);
+
+    // Move the cursor position after the inserted curly braces
+    codeInput.selectionStart = codeInput.selectionEnd = start + 1;
   }
 });
